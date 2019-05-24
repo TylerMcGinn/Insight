42f6646e5bb221ad9b6d56a6e7a8ffb019e15eff
Tyler McGinn <systematyler@gmail.com>
2019-05-24 16:46:23 -0700

    treeview works and can open url's.

--- src/Academy.ts
+++ src/Academy.ts
@@ -3,16 +3,51 @@ import * as vscode from 'vscode';
 export class MenuDataProvider implements vscode.TreeDataProvider<MenuModel>{
     
     getTreeItem(element: MenuModel): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        throw new Error("Method not implemented.");
+        return element;
     }
 
-    getChildren(element?: MenuModel | undefined): vscode.ProviderResult<MenuModel[]> {
-        throw new Error("Method not implemented.");
+    getChildren(element?: MenuModel | undefined): MenuModel[] {
+        let testObj = [
+            {
+            label:"Youtube",
+            websiteURL:"https://www.youtube.com",
+            collapsibleState:vscode.TreeItemCollapsibleState.None
+            }
+        ];
+        let menuItems:MenuModel[] = [];
+        if(testObj.length !== 0){
+            for(let i = 0; i<testObj.length; i++){
+                let label = testObj[i].label;
+                let websiteURL = testObj[i].websiteURL;
+                let collapsibleState = testObj[i].collapsibleState;
+
+                menuItems[i] = new MenuModel(
+                    label, 
+                    websiteURL, 
+                    collapsibleState, 
+                    {
+                        command:"Extension.goToSite",
+                        title:"",
+                        arguments:[websiteURL]
+                    });
+            }
+        }
+        return menuItems;    
     }
-
-
 }
 
 class MenuModel extends vscode.TreeItem{
-
+    websiteURL:string;
+    constructor(
+        label:string,
+        websiteURL:string,
+        collapsibleState:vscode.TreeItemCollapsibleState,
+        command?:vscode.Command
+    ) {
+        super(label, collapsibleState);
+        this.label = label;
+        this.websiteURL = websiteURL;
+        this.collapsibleState = collapsibleState;
+        this.command = command;
+    }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,10 @@
 import * as vscode from 'vscode';
+import {MenuDataProvider} from './Academy';
 
 export function activate(context: vscode.ExtensionContext) {
-
-
+let provider = new MenuDataProvider;
+vscode.window.registerTreeDataProvider('AcademyMenu', provider);
+vscode.commands.registerCommand('Extension.goToSite',(websitUrl)=>vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(websitUrl)));
 
 }
 
